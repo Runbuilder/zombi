c139674fed0fdbb4e2b1bc65b0bb036cd83ac679
runbuilder <154466890+Runbuilder@users.noreply.github.com>
2024-07-28 07:30:39 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'zombie-sf-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',
+  '/script.js',
+  '/images/turret.png',
+  '/images/bullet_normal.png',
+  // 추가할 모든 리소스
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
